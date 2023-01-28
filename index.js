8406142c7073337769c1a6b6754e460c91c9c1c2
Akash <na.akash715@gmail.com>
2023-01-29 03:12:24 +0600

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const places = require('./data/places.json');
+const hotels = require('./data/hotels.json');
 
 app.get('/', (req, res) => {
     res.send('What a trip API');
@@ -20,6 +21,16 @@ app.get('/places/:id', (req, res) => {
     const id = req.params.id;
     const selectedPlace = places.find(place => place.id == id);
     res.send(selectedPlace);
+});
+
+app.get('/hotels', (req, res) => {
+    res.send(hotels);
+});
+
+app.get('/hotels/:categoryId', (req, res) => {
+    const categoryId = req.params.categoryId;
+    const foundHotels = hotels.filter(hotel => hotel.category_id === categoryId);
+    res.send(foundHotels);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
